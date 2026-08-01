2ca9bdea7c016626a6b299621f4fecaba14932b5
Paulo Rodrigues Moraes <>
2026-08-01 13:56:20 -0300

    Release 1.3.5

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3539203680"
+    serviceWorkerVersion: "1672579910"
   }
 });
